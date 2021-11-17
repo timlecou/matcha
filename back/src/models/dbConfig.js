9145e7e2251ca4f8f672e36a0a0d234d679768c4
timlecou <lecouvreur.timithe@gmail.com>
2021-11-17 15:00:53 +0100

    config db js

--- back/src/models/dbConfig.js
+++ back/src/models/dbConfig.js
@@ -1,10 +1,12 @@
 var pgp = require("pg-promise")(/*options*/);
 var db = pgp("postgres://postgres:password@website-db:5432");
 
-db.one("SELECT $1 AS value", 123)
+db.one("SELECT $1 AS value", 42)
     .then(function (data) {
         console.log("DATA:", data.value);
     })
     .catch(function (error) {
         console.log("ERROR:", error);
-    });
+});
+
+console.log(db.any.name)
