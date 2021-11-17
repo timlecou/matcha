9e9f03ae26c2220e19beddd18640a66eb1a56138
Timothe LECOUVREUR <timlecou@e1r10p8.42.fr>
2021-11-17 12:16:34 +0100

    postgres connected

--- back/src/app.js
+++ back/src/app.js
@@ -1,12 +1,20 @@
-let app = require('express')()
+/**
+ * Requires
+ */
+const express = require('express')()
+const cors = require('cors')
 
-var cors = require('cors')
+/**
+ * connection to the database
+ */
+require('./models/dbConfig.js')
 
+let app = express
 app.use(cors())
 
 app.get('/', async (request, response) => {
-    console.log('request recieved', request)
-    response.send('salut gros ca va ?')
+    console.log('request recieved')
+    response.send('salut gros')
 })
 
-app.listen(4000)
+app.listen(4000, () => console.log('Server started on http://localhost:4000'))
