76898c3ff7649a7571cc2fc9e91ab2e3bcd433de
Yassine <yassou200121@gmail.com>
2021-11-18 11:49:36 +0100

    fix phpgadmin

--- back/src/models/dbConfig.js
+++ back/src/models/dbConfig.js
@@ -1,4 +1,4 @@
 var pgp = require("pg-promise")(/*options*/);
-var db = pgp("postgres://user:mypassword@website-db:5432/matcha");
+var db = pgp("postgres://user:mypassword@db:5432/matcha");
 
-console.log(db.any("SELECT * FROM company").toString())
+// console.log(db.any("SELECT * FROM company").toString())
