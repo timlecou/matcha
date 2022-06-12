c3aef1325e4d9c83aab0aacfdbf5d12d0e6f345e
Yassine <yassou200121@gmail.com>
2022-06-12 18:38:06 +0200

    Install vue-observe-visibility to check when an element is visible and and vue-scrollto

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -23,6 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/VueObserveVisibility.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -30,13 +31,14 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    "@nuxtjs/svg"
+    "@nuxtjs/svg",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'vue-scrollto/nuxt',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
---
+++ front/plugins/VueObserveVisibility.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import VueObjserveVisibility from 'vue-observe-visibility';
+
+Vue.use(VueObjserveVisibility);
